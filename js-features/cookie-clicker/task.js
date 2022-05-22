b914ab39c1282531017eb5e90bc2a5a6a3a7cf52
Ripacheva Anastasia <riaaaaa@yandex.ru>
2022-05-22 23:44:42 +0300

    возврат в фун-ии необходимого размера, добавлен счетчик

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -8,13 +8,22 @@ cookie.onclick = function() {
   }
 }*/
 const cookie = document.getElementById("cookie");
+
+
 function changeSizes() {
   if (cookie.width === 200) {
-    cookie.width = 250;
+    return cookie.width = 250;
   }
-  /*if (cookie.width === 250) {
-    cookie.width = 200;
-  }*/
-  
+  if (cookie.width === 250) {
+    return cookie.width = 200;
+  }
 };
-cookie.onclick = changeSizes;
+
+function clickCounter() {
+  const counter = document.getElementById("clicker__counter");
+  return counter.textContent++;
+}
+  
+cookie.addEventListener("click", changeSizes);
+cookie.addEventListener("click", clickCounter); 
+//cookie.onclick = (clickCounter);
