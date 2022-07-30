2f4ee727f85ffdd78d06610291e174c9ffdd23e3
Ripacheva Anastasia <riaaaaa@yandex.ru>
2022-07-30 17:50:49 +0300

    замена на тернарный оператор

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,17 +1,18 @@
 const cookie = document.getElementById("cookie");
-function changeSizes() {
+/*function changeSizes() {
   if (cookie.width === 200) {
     return cookie.width = 250;
   }
   if (cookie.width === 250) {
     return cookie.width = 200;
   }
-};
+};*/
 
 function clickCounter() {
   const counter = document.getElementById("clicker__counter");
-  return counter.textContent++;
+  cookie.width = ++counter.textContent % 2 ? 250 : 200;
+  return counter.textContent;
 }
   
-cookie.addEventListener("click", changeSizes);
+//cookie.addEventListener("click", changeSizes);
 cookie.addEventListener("click", clickCounter); 
